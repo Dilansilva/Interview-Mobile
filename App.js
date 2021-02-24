fbacd3e83124de4cc82c2f4096d96c4072d055bc
Dilan Silva <sdilansilva@gmail.com>
2021-02-25 00:19:16 +0530

    Initial Commits

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//make
 
 export default function App() {
   return (
